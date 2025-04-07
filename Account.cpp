b41cab3ed7407adc24c5d80d4714e04a0e4ead59
Kate Eggenberg <keggenb@jacksonville.edu>
2025-04-07 12:00:07 -0400

    account header, checking cpp, start of checking cpp, and source testing

--- Account.cpp
+++ Account.cpp
@@ -1,129 +1,115 @@
-#include "Customer.h"
+
 #include "Account.h"
-#include <iostream>
-using namespace std;
-#include <string>
 
-class Account
+int Account::getId() const
 {
-	int id;
-	double balance;
-	int withdrawalCounter = 0, depositsCounter = 0;
-	Customer* accountCustomer;
+	return id;
+}
 
-public:
-	int Account::getId() const
+double Account::getBalance() const
+{
+	return balance;
+}
+
+int Account::getwithdrawalCounter() const
+{
+	return withdrawalCounter;
+}
+int Account::getDepositsCounter() const
+{
+	return depositsCounter;
+}
+Customer* Account::getAccountCustomer()
+{
+	return accountCustomer;
+}
+
+void Account::setId(int _id)
+{
+	if (_id > 0)
 	{
-		return id;
+		id = _id;
 	}
-
-	double Account::getBalance() const
-	{
-		return balance;
-	}
-
-	int Account::getwithdrawalCounter() const
-	{
-		return withdrawalCounter;
-	}
-	int Account::getDepositsCounter() const
-	{
-		return depositsCounter;
-	}
-	Customer* Account::getAccountCustomer()
-	{
-		return accountCustomer;
-	}
-
-	void Account::setId(int _id)
-	{
-		if (_id > 0)
-		{
-			id = _id;
-		}
-		else
-		{
-			id = 0;
-		}
-	}
-	void Account::setBalance(double _balance)
-	{
-		balance = _balance;
-	}
-
-	void Account::setCustomer(Customer* cPtr)
-	{
-		accountCustomer = cPtr;
-	}
-
-
-
-	void Account::setAll(Customer* cPtr, int _id, double _balance)
-	{
-		accountCustomer = cPtr;
-
-		if (_id > 0)
-		{
-			id = _id;
-		}
-		else
-		{
-			id = 0;
-		}
-		balance = _balance;
-	}
-
-	Account::Account()
+	else
 	{
 		id = 0;
-		balance = 0.0;
-		withdrawalCounter = 0;
-		depositsCounter = 0;
-		accountCustomer = nullptr;
-
 	}
-	Account::Account(Customer * cPtr, int _id, double _balance)
+}
+void Account::setBalance(double _balance)
+{
+	balance = _balance;
+}
+
+void Account::setCustomer(Customer* cPtr)
+{
+	accountCustomer = cPtr;
+}
+
+
+
+void Account::setAll(Customer* cPtr, int _id, double _balance)
+{
+	accountCustomer = cPtr;
+
+	if (_id > 0)
 	{
-		SetAll(cPtr, _id, _balance);
+		id = _id;
 	}
-
-	void Account::depositMoney(double a)
+	else
 	{
-		if (a >= 0)
-		{
-			balance = a;
-			depositsCounter++;
-		}
-		else
-		{
-			cout << "*ERROR NEGATIVE DEPOSIT AMOUNT*"
-		}
+		id = 0;
 	}
+	balance = _balance;
+}
 
-	void Account::withdrawMoney(double w)
+Account::Account()
+{
+	id = 0;
+	balance = 0.0;
+	withdrawalCounter = 0;
+	depositsCounter = 0;
+	accountCustomer = nullptr;
+
+}
+Account::Account(Customer* cPtr, int _id, double _balance)
+{
+	SetAll(cPtr, _id, _balance);
+}
+
+void Account::depositMoney(double a)
+{
+	if (a >= 0)
 	{
-		if (w <= balance)
-		{
-			balance = balance - w;
-			withdrawalCounter++;
-		}
-		else
-		{
-			cout << "*INSUFFICIENT FUNDS*"
-		}
+		balance = a;
+		depositsCounter++;
 	}
-
-	void Account::PrintInfo(double a)
+	else
 	{
-		cout << setw(15) << "ID" << setw(15) << "Balance" << setw(20) << "Withdrawals Made" << setw(15) << "Deposits Made"<< endl;
-		for (int i = 0; i < 50; i++)
-		{
-			cout << "=";
-		}
-		cout << endl;
-		cout << setw(15) << id << setw(15) << balance << setw(20) << withdrawalCounter << setw(15) << depositsCounter << endl;
-		accountCustomer.PrintInfo();
+		cout << "*ERROR NEGATIVE DEPOSIT AMOUNT*"
 	}
+}
 
+void Account::withdrawMoney(double w)
+{
+	if (w <= balance)
+	{
+		balance = balance - w;
+		withdrawalCounter++;
+	}
+	else
+	{
+		cout << "*INSUFFICIENT FUNDS*"
+	}
+}
 
-};
+void Account::PrintInfo(double a)
+{
+	cout << setw(15) << "ID" << setw(15) << "Balance" << setw(20) << "Withdrawals Made" << setw(15) << "Deposits Made" << endl;
+	for (int i = 0; i < 50; i++)
+	{
+		cout << "=";
+	}
+	cout << endl;
+	cout << setw(15) << id << setw(15) << balance << setw(20) << withdrawalCounter << setw(15) << depositsCounter << endl;
+	accountCustomer.PrintInfo();
+}
--- CheckingAccount.cpp
+++ CheckingAccount.cpp
@@ -1,6 +1,10 @@
 #include "Account.h"
+#include "CheckingAccount.h"
+#include <string>
+#include <iostream>
 
-class CheckingAccount :: Account()
+
+class CheckingAccount : public Account
 {
 	double overDraftLimit;
 
@@ -34,4 +38,73 @@ public:
 		Account::Account(_id, _balance, _customer);
 		overDraftLimit = _overDraftLimit;
 	}
+	void WithdrawMoney(double amount)
+	{
+		if (amount > 0 && getBalance() - amount > 0)
+		{
+			Account::WithdrawMoney(amount);
+
+		}
+		else if (abs(getBalance() - amount) <= overDraftLimit)
+		{
+			cout << "Overdraft Warning!" << endl;
+			cout << "Fee: $20" << endl;
+			Account::WithdrawMoney(amount + 20);
+		}
+		else if (abs(getBalance() - amount) > overDraftLimit)
+		{
+			cout << "Withdrawal not allowed!" << endl;
+			cout << "Withdrawal amount over over draft limit." << endl;
+		}
+	}
+
 };
+
+void CheckingAccount::setOverDraftLimit(double _overDraftLimit)
+{
+	if (_overDraftLimit <= 100)
+	{
+		overDraftLimit = _overDraftLimit;
+	}
+	else
+	{
+		overDraftLimit = 100.0;
+	}
+
+}
+double CheckingAccount::getOverDraftLimit() const
+{
+	return overDraftLimit;
+}
+CheckingAccount::CheckingAccount() : Account()
+{
+	overDraftLimit = 0.0;
+}
+CheckingAccount::CheckingAccount(int _id, double _balance, Customer* _customer, double _overDraftLimit) : Account(_id, _balance, _withdrawCounter, _depositsCounter, _customer)
+{
+	setOverDraftLimit(_overDraftLimit);
+}
+void CheckingAccount::setAll(int _id, double _balance, Customer* _customer, double _overDraftLimit)
+{
+	Account::Account(_id, _balance, _customer);
+	overDraftLimit = _overDraftLimit;
+}
+void CheckingAccount::WithdrawMoney(double amount)
+{
+	if (amount > 0 && getBalance() - amount > 0)
+	{
+		Account::WithdrawMoney(amount);
+
+	}
+	else if (abs(getBalance() - amount) <= overDraftLimit)
+	{
+		cout << "Overdraft Warning!" << endl;
+		cout << "Fee: $20" << endl;
+		Account::WithdrawMoney(amount + 20);
+	}
+	else if (abs(getBalance() - amount) > overDraftLimit)
+	{
+		cout << "Withdrawal not allowed!" << endl;
+		cout << "Withdrawal amount over over draft limit." << endl;
+	}
+}
