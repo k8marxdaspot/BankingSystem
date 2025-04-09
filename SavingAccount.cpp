4d3d53634ac10ab95ad890fe097e7c35f3985f5f
Kate Eggenberg <keggenb@jacksonville.edu>
2025-04-09 11:43:34 -0400

    created SavingAccount cpp and SavingAccount header file

---
+++ SavingAccount.cpp
@@ -0,0 +1,51 @@
+#include "SavingAccount.h"
+
+void SavingAccount::setInterestRate(double _interestRate)
+{
+	if (interestRate > 0.0 && interestRate < 10.0) // interest rate should not be 0 and it would be very unlikely for it to be greater than 10
+	{											   // high yield savings accounts have around a maximum interest rate of 5ish	
+		interestRate = _interestRate;
+	}
+	else
+	{
+		interestRate = 0.0;
+	}
+}
+
+double SavingAccount::getInterestRate() const
+{
+	return interestRate;
+}
+
+SavingAccount::SavingAccount() : Account()
+{
+	interestRate = 0.0;
+}
+
+SavingAccount::SavingAccount(Customer * _customer, int _id, double _balance, double _interestRate) : Account(_customer, _id, _balance)
+{
+	setInterestRate(_interestRate);
+}
+
+void SavingAccount::setAll(Customer * _customer, int _id, double _balance, double _interestRate)
+{
+	Account::setAll(_customer, _id, _balance);
+	setInterestRate(_interestRate);
+}
+
+void SavingAccount::payInterest()
+{
+	if (getBalance() > 0)
+	{
+		double interestAccrued = getBalance() * interestRate;
+		depositMoney(interestAccrued);
+	}
+}
+
+void SavingAccount::transfer(double amount, SavingAccount* destinationAccount)
+{
+	if (getBalance() - amount >= 0.0)
+	{
+		destinationAccount->depositMoney(amount);
+	}
+}
--- SavingAccount.h
+++ SavingAccount.h
@@ -1,1 +1,18 @@
 #pragma once
+#ifndef SAVINGACCOUNT
+#include "Account.h"
+
+class SavingAccount : public Account
+{
+	double interestRate;
+public:
+	void setInterestRate(double _interestRate);
+	double getInterestRate() const;
+	SavingAccount();
+	SavingAccount(Customer * _customer, int _id, double _balance, double _interestRate);
+	void setAll(Customer * _customer, int _id, double _balance, double _interestRate);
+	void payInterest();
+	void transfer(double amount, SavingAccount* destinationAccount);
+};
+#endif // !SAVINGACCOUNT
+
