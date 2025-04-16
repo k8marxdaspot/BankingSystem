9be3309535f53c6de57482f66c901b3845dae03e
Kate <71048152+k8marxdaspot@users.noreply.github.com>
2025-04-16 11:09:27 -0400

    Update Source.cpp
    option 2

--- Source.cpp
+++ Source.cpp
@@ -113,7 +113,78 @@ int main()
 		}
 		else if (userOpt == 2)
 		{
-	
+			string custFname;
+			string custLname;
+			string userAddress;
+			string custEmail;
+			string custPhone;
+			double custBalance;
+			double custInterestRate;
+			Customer* cPtr = nullptr;
+			bool existingCustomer = false;
+			savingTracker++;
+
+			if (savingTracker < 20) // might give us issues later if somehow savingTracker isn't over 20 but all slots are filled thus causing us to overwrite someone's savings account
+			{
+				cout << "Please input email: ";
+				cin.ignore();
+				getline(cin, custEmail);
+
+				cout << "Please input phone number:";
+				getline(cin, custPhone);
+
+				for (int i = 0; i < sizeof(customerTracker); i++) // search for existing customer with matching phone number or email
+				{
+					if (customerList[i].getEmail() == custEmail || customerList[i].getPhone() == custPhone) // found existing customer 
+					{
+						cout << "Customer found: " << customerList[i].getFname() << " " << customerList[i].getLname() << endl;
+						cout << "Please input balance for new savings account: ";
+						cin >> custBalance;
+						cout << "Please input interest rate for new savings account: ";
+						cin >> custInterestRate;
+						Customer* cPtr = customerList + i;
+						savingList[checkingTracker].setAll(cPtr, i, custBalance, custInterestRate);
+						existingCustomer = true;
+					}
+				}
+
+				if (existingCustomer == false) // did not find existing customer account and thus must create customer and saving account
+				{
+					customerTracker++;
+					if (customerTracker < 20 && (customerList[customerTracker].getFname() == "" && customerList[customerTracker].getLname() == "" &&
+						customerList[customerTracker].getAddress() == "" && customerList[customerTracker].getPhone() == "" &&
+						customerList[customerTracker].getEmail() == "")) // makes sure that customerList array has room for another customer and that the customer in the array that the tracker is on is empty!
+					{
+						cout << "Please input first name: ";
+						cin >> custFname;
+						cout << "Please input last name: ";
+						cin >> custLname;
+						cout << "Please input customer's address: ";
+						cin.ignore();
+						getline(cin, userAddress);
+						customerList[customerTracker].setAll(custFname, custLname, userAddress, custPhone, custEmail);
+
+						cout << "Please input customer's balance: ";
+						cin.ignore();
+						cin >> custBalance;
+						cout << "Please input interest rate for customer: ";
+						cin >> custInterestRate;
+						Customer* cPtr = customerList + customerTracker;
+						savingList[checkingTracker].setAll(cPtr, checkingTracker, custBalance, custInterestRate);
+					}
+					else // either no room for customer or element in customer list has preexisting information occupying it
+					{
+						cout << "Out of Room For More Customers!" << endl;
+						break;
+					}
+				}
+
+			}
+			else
+			{
+				cout << "Sorry We Are Out of Saving Account Space!" << endl;
+				break;
+			}
 		}
 		else if (userOpt == 3)
 		{
@@ -141,9 +212,9 @@ int main()
 		cout << "Please choose an option:" << endl;
 		cout << "1. CREATE A CHECKING ACCOUNT" << endl;
 		cout << "2. CREATE A SAVINGS ACCOUNT" << endl;
-		cout << "3. VIEW MY ACCOUNT INFORMATION" << endl;
-		cout << "4. MODIFY MY ACCOUNT" << endl;
-		cout << "5. DELETE MY ACCOUNT" << endl;
+		cout << "3. VIEW ACCOUNT INFORMATION" << endl;
+		cout << "4. MODIFY ACCOUNT" << endl;
+		cout << "5. DELETE ACCOUNT" << endl;
 		cout << "6. TRANSFER" << endl;
 		cout << "7. QUIT" << endl;
 		cin.ignore();
