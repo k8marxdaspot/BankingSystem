8473f3485e5ef8844337df207e4bf31afe63c044
Kate <71048152+k8marxdaspot@users.noreply.github.com>
2025-04-20 19:36:36 -0400

    pushing so i can pull from git

---
+++ Password.h
@@ -0,0 +1,12 @@
+#pragma once
+#ifndef PASSWORD
+#include <string>
+using namespace std;
+
+class Password
+{
+	string employeeUsers[10] = {"jmoran10", "kfields56", "jfarnberg", "krashid5", "mrobinson47", "fdostoevsky", "nklein08", "aibrahim15", "erobanvitch", "szizek12"};
+	string employeePasswords[10] = { "admin1", "ILoveYou", "p4ssw0rd", "REDFERRARI", "#1dodgersfan", "crimeandpunishment", "", "", "", ""};
+};
+#endif // !PASSWORD
+
